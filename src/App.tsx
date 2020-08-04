03928136eb53172cdd5cdcf58be3563f2ef3c685
Haris <muhamad_haris@hotmail.com>
2020-08-04 17:29:17 +0800

    💄 Add container div with switch, router and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Flutter learning platform</h1>
+      <Router>
+        <NavBar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={HomePage}></Route>
+            <Route exact path="/login" component={LoginPage}></Route>
+            <Route exact path="/signup" component={SignUpPage}></Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
