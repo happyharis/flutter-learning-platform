ecfda68daa0217770e56db5cfd6034aab667f1ff
Haris <muhamad_haris@hotmail.com>
2020-08-04 15:08:52 +0800

    📦 Add home, login and sign up pages

---
+++ src/pages/home_page.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>HomePage</h1>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
