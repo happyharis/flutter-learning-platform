53c510f1247de7751e8218dd100a859ea5624f5d
Haris <muhamad_haris@hotmail.com>
2020-08-04 17:30:02 +0800

    💄 Add navbar buttons with links
    Links goes to home, login and sign up page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
   render() {
     return (
-      <div>
-        <h1>NavBar</h1>
-      </div>
+      <AppBar>
+        <Toolbar>
+          <Button color="inherit" component={Link} to="login">
+            Login
+          </Button>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="signup">
+            Sign Up
+          </Button>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
